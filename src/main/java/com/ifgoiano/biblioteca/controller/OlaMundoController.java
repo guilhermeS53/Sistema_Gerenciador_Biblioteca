4a04901881aee77222780289041790bea865e2e8
Guilherme Sousa <guilherme.gs17@outlook.com>
2024-05-18 10:21:36 -0300

    fix: ajuste de importação da classe OlaMundoController

--- src/main/java/com/ifgoiano/biblioteca/controller/OlaMundoController.java
+++ src/main/java/com/ifgoiano/biblioteca/controller/OlaMundoController.java
@@ -2,15 +2,13 @@ package com.ifgoiano.biblioteca.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 public class OlaMundoController {
-    
+
     @GetMapping("/")
-    
-    public String olaMundo(){
+
+    public String olaMundo() {
         return "Olá Mundo! Isso no SpringBoot do VSCode";
     }
 }
