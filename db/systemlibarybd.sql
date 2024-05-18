9f642336736d8ce5f7899ef65b6161838ec633f8
Guilherme Sousa <guilherme.gs17@outlook.com>
2024-05-18 09:03:45 -0300

    Adjust: DB Project

--- db/systemlibarybd.sql
+++ db/systemlibarybd.sql
@@ -1,3 +1,8 @@
+CREATE TABLE categoria (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE autor (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL
@@ -8,15 +13,10 @@ CREATE TABLE editora (
     nome VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE categoria (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL
-);
-
 CREATE TABLE resenha (
     id SERIAL PRIMARY KEY,
-    texto TEXT
-    /*livro_id INT REFERENCES livro(id)*/
+    texto TEXT,
+    nota INTEGER
 );
 
 CREATE DOMAIN cpf AS VARCHAR(11) CHECK (VALUE ~ '^[0-9]{11}$');
@@ -27,7 +27,7 @@ CREATE TABLE usuario (
     email VARCHAR(255) NOT NULL,
     cpf VARCHAR(11) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL,
-	quantidade_emprestimos INTEGER DEFAULT 0
+    quantidade_emprestimos INTEGER DEFAULT 0
 );
 
 CREATE DOMAIN nota AS INTEGER CHECK (VALUE >= 0 AND VALUE <= 5);
@@ -35,29 +35,26 @@ CREATE DOMAIN nota AS INTEGER CHECK (VALUE >= 0 AND VALUE <= 5);
 CREATE TABLE avaliacao (
     id SERIAL PRIMARY KEY,
     comentario VARCHAR(255),
-    nota INTEGER,
+    nota INTEGER CHECK (nota >= 0 AND nota <= 5),
     usuario_id INTEGER,	
-	livro_id INTEGER,
-	FOREIGN KEY(usuario_id) REFERENCES usuario (id) ON DELETE CASCADE ON UPDATE CASCADE	
+    livro_id INTEGER,
+    FOREIGN KEY(usuario_id) REFERENCES usuario (id) ON DELETE CASCADE ON UPDATE CASCADE	
 );
-
 
 CREATE TABLE livro (
     id SERIAL PRIMARY KEY,
-	isbn VARCHAR(13) NOT NULL UNIQUE,
+    isbn VARCHAR(13) NOT NULL UNIQUE,
     nome VARCHAR(255) NOT NULL,
     quantidade INT,
     ano INT,
-	id_autor INTEGER,
-	id_editora INTEGER,
-	id_categoria INTEGER,
-	id_avaliacao INTEGER,
-	id_resenha INTEGER, 
-	FOREIGN KEY(id_autor) REFERENCES autor (id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY(id_editora) REFERENCES editora (id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY(id_categoria) REFERENCES categoria (id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY(id_avaliacao) REFERENCES avaliacao (id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY(id_resenha) REFERENCES resenha (id) ON DELETE CASCADE ON UPDATE CASCADE
+    id_autor INTEGER,
+    id_editora INTEGER,
+    id_categoria INTEGER,
+    id_resenha INTEGER,
+    FOREIGN KEY(id_autor) REFERENCES autor (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(id_editora) REFERENCES editora (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(id_categoria) REFERENCES categoria (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(id_resenha) REFERENCES resenha (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 ALTER TABLE avaliacao
@@ -69,13 +66,12 @@ CREATE TABLE emprestimo (
     id SERIAL PRIMARY KEY,
     data_emprestimo DATE NOT NULL,
     data_prev_devolucao DATE GENERATED ALWAYS AS 
-	(data_emprestimo + INTERVAL '15 days') STORED,
+    (data_emprestimo + INTERVAL '15 days') STORED,
     data_real_devolucao DATE,
     devolucao VARCHAR(11),
     multa INTEGER DEFAULT 0,
     usuario_id INTEGER,
-	livro_id INTEGER,
-	FOREIGN KEY(usuario_id) REFERENCES usuario (id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY(livro_id) REFERENCES livro (id) ON DELETE CASCADE ON UPDATE CASCADE	
+    livro_id INTEGER,
+    FOREIGN KEY(usuario_id) REFERENCES usuario (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(livro_id) REFERENCES livro (id) ON DELETE CASCADE ON UPDATE CASCADE	
 );
-
