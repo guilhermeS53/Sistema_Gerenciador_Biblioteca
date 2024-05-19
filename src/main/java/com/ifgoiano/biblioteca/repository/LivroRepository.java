f405d158eef203a29cfbda4b83f09a7d3f96fd86
Guilherme Sousa <guilherme.gs17@outlook.com>
2024-05-19 14:55:21 -0300

    feat: adicionada a biblioteca do JPA Repository

--- src/main/java/com/ifgoiano/biblioteca/repository/LivroRepository.java
+++ src/main/java/com/ifgoiano/biblioteca/repository/LivroRepository.java
@@ -2,8 +2,10 @@ package com.ifgoiano.biblioteca.repository;
 
 import com.ifgoiano.biblioteca.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 // Criação da interface LivroRepository do repositório que estendem JpaRepository
+@Repository
 public interface LivroRepository extends JpaRepository<Livro, Long> {
 
 }
--- src/main/java/com/ifgoiano/biblioteca/repository/UsuarioRepository.java
+++ src/main/java/com/ifgoiano/biblioteca/repository/UsuarioRepository.java
@@ -2,8 +2,10 @@ package com.ifgoiano.biblioteca.repository;
 
 import com.ifgoiano.biblioteca.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 // Criação da interface UsuarioRepository do repositório que estendem JpaRepository
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 }
