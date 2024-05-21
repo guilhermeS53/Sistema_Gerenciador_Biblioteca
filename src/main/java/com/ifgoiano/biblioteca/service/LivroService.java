46c8b6be01668f9e9eb47b187330a511a639c4c0
Guilherme Sousa <guilherme.gs17@outlook.com>
2024-05-21 19:32:43 -0300

    fix: implementação das interfaces criadas

--- src/main/java/com/ifgoiano/biblioteca/service/LivroService.java
+++ src/main/java/com/ifgoiano/biblioteca/service/LivroService.java
@@ -10,31 +10,37 @@ import com.ifgoiano.biblioteca.repository.LivroRepository;
 
 // Criação do serviço que encapsula a lógica de negócios
 @Service
-public class LivroService {
+public class LivroService implements ILivroService {
 
     @Autowired
     private LivroRepository livroRepository;
 
+    @Override
     public List<Livro> findAll() {
         return livroRepository.findAll();
     }
 
+    @Override
     public Livro findById(Long id) {
         return livroRepository.findById(id).orElse(null);
     }
 
+    @Override
     public List<Livro> findByTituloContaining(String titulo) {
         return livroRepository.findByTituloContainingIgnoreCase(titulo);
     }
 
+    @Override
     public Livro save(Livro livro) {
         return livroRepository.save(livro);
     }
 
+    @Override
     public void deleteById(Long id) {
         livroRepository.deleteById(id);
     }
 
+    @Override
     public Livro updateLivro(Long id, Livro livroDetails) {
         Livro livro = findById(id);
         if (livro != null) {
@@ -47,6 +53,7 @@ public class LivroService {
         return null;
     }
 
+    @Override
     public Livro emprestarLivro(Long id) {
         Livro livro = findById(id);
         if (livro != null && !livro.isEmprestado()) {
@@ -56,10 +63,11 @@ public class LivroService {
         return null;
     }
 
+    @Override
     public Livro devolverLivro(Long id) {
         Livro livro = findById(id);
         if (livro != null && livro.isEmprestado()) {
-            livro.setEmprestado(false);
+            livro.setEmprestado(false); 
             return save(livro);
         }
         return null;
--- src/main/java/com/ifgoiano/biblioteca/service/UsuarioService.java
+++ src/main/java/com/ifgoiano/biblioteca/service/UsuarioService.java
@@ -9,27 +9,32 @@ import com.ifgoiano.biblioteca.repository.UsuarioRepository;
 
 // Criação do serviço de Usuário que encapsula a lógica de negócios
 @Service
-public class UsuarioService {
+public class UsuarioService implements IUsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Override
     public Usuario save(Usuario usuario) {
         return usuarioRepository.save(usuario);
     }
 
+    @Override
     public List<Usuario> findAll() {
         return usuarioRepository.findAll();
     }
 
+    @Override
     public Usuario findById(Long id) {
         return usuarioRepository.findById(id).orElse(null);
     }
 
+    @Override
     public void deleteById(Long id) {
         usuarioRepository.deleteById(id);
     }
 
+    @Override
     public Usuario authenticate(String login, String senha) {
         return usuarioRepository.findByLoginAndSenha(login, senha).orElse(null);
     }
