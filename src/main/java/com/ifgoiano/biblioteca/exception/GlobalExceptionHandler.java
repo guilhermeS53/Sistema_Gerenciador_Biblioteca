bb3e438dc47cef007f63ef498d004e85f678894d
Guilherme Sousa <guilherme.gs17@outlook.com>
2024-05-27 20:01:33 -0300

    feat: criação de um manipulador de exceções globais

---
+++ src/main/java/com/ifgoiano/biblioteca/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.ifgoiano.biblioteca.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.ifgoiano.biblioteca.model.ResourceNotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
