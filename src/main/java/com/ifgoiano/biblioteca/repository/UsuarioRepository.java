1bfe06ffda1d6630a0751a8918248e210f8184bc
Guilherme Sousa <guilherme.gs17@outlook.com>
2024-05-30 21:40:48 -0300

    remove: remoção de importação não usada

--- src/main/java/com/ifgoiano/biblioteca/repository/UsuarioRepository.java
+++ src/main/java/com/ifgoiano/biblioteca/repository/UsuarioRepository.java
@@ -2,8 +2,6 @@ package com.ifgoiano.biblioteca.repository;
 
 import com.ifgoiano.biblioteca.model.Usuario;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
